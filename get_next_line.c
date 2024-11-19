2a3cef995c7f8e714b87260881e8fd045c2e3546
Paula <paula.gonser@icloud.com>
2024-11-19 09:33:14 +0100

    last commit

--- get_next_line.c
+++ get_next_line.c
@@ -8,11 +8,13 @@ char *extract_line(char *aux_read)
     i = 0;
     while (aux_read[i] != '\0' && aux_read[i] != '\n')
         i++;
+    if (aux_read[i] == '\n')
+        i++;
     aux = malloc((i + 1) * sizeof(char));
     if(!aux)
-        return(0);
+        return(NULL);
     i = 0;
-    while (aux_read[i] != '\0' && aux_read [i]!= '\n')
+    while (aux_read[i] != '\0' && (i == 0 || aux_read [i - 1] != '\n'))
     {
         aux[i] = aux_read[i];
         i++;
@@ -33,16 +35,15 @@ char *remove_first_line(char *statica)
     j = 0;
     while (statica[i] != '\0' && statica[i] != '\n')
         i++;
+    if (statica[i] == '\0')
+        return (free(statica), NULL);
     i++;
     k = i;
-    while (statica[i] != '\0')
-    {
-        i++;
+    while (statica[i++] != '\0')
         len_new_static++;
-    }
-    new_static = malloc ((len_new_static + 1) * sizeof(char));
-    if(!new_static)
-        return (0);
+    new_static = malloc((len_new_static + 1) * sizeof(char));
+    if(new_static == NULL)
+        return (free(statica), NULL);
     while (statica[k] != '\0')
         new_static[j++] = statica[k++];
     new_static[j] = '\0';
@@ -53,67 +54,42 @@ char *remove_first_line(char *statica)
 char *get_read(int fd, char *statica)
 {
     int val_read;
-    char aux_read[BUFFER_SIZE];
+    char aux_read[BUFFER_SIZE + 1];
     
     val_read = 1;
     if(statica == NULL)
     {
-        statica = malloc(BUFFER_SIZE *sizeof(char));
+        statica = malloc(BUFFER_SIZE * sizeof(char));
         if(!statica)
             return (0);
         statica[0] = '\0';
     }
-    while(val_read != 0) //&& !mystrchr(statica, '\n'))
+    while(val_read != 0 && !mystrchr(statica, '\n'))
     {
         val_read = read(fd, aux_read, BUFFER_SIZE);
+        if (val_read < 0)
+            return (free(statica), NULL);
         aux_read[val_read] = '\0';
         statica = mystrjoin(statica, aux_read);
+        if (!statica)
+            return (NULL);
     }
     return(statica);
 }
+
 char *get_next_line(int fd)
 {
-    static char *statica;
+    static char *statica = NULL;
     char *line;
 
-    line = NULL;
-    if (fd < 0 || BUFFER_SIZE <= 0)
-	{	
-		free(statica);
-		statica = NULL;
-		return (NULL);
-	}
     statica = get_read(fd, statica);
+    if (statica == NULL)
+        return (NULL);
+    if(statica[0] == '\0')   
+        return (free(statica), statica = NULL);
     line = extract_line(statica);
+    if (line == NULL)
+        return (free(statica), statica = NULL);
     statica = remove_first_line(statica);
-    if(line && *line == '\0')
-    {
-            free(line);
-            free(statica);
-            line= NULL;
-            statica = NULL;
-    }
     return (line);
 }
-
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*str;
-// 	int		i;
-
-// 	i = 0;
-// 	fd = open("hola", O_RDONLY);
-// 	while ((i <= 6))
-// 	{
-// 		str = get_next_line(fd);
-// 		printf("line %i => [%s]\n", i + 1, str);
-// 		if (!str)
-// 			break;
-// 		free(str);
-// 		i++;
-// 	}
-// 	//printf("BUFFER_SIZE = %d\n", BUFFER_SIZE);
-// 	return (0);
-// }
-
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -19,9 +19,9 @@ char *mystrjoin(char *statica, char *aux_read)
     
     i = 0;
     j = 0;
-    new_static = malloc ((mystrlen(statica) + mystrlen(aux_read) + 1) *sizeof(char));
+    new_static = malloc((mystrlen(statica) + mystrlen(aux_read) + 1) *sizeof(char));
     if (!new_static)
-        return (0);
+        return (free(statica), NULL);
     while (statica[i])
     {
         new_static[i] = statica[i];
@@ -34,16 +34,16 @@ char *mystrjoin(char *statica, char *aux_read)
     return(new_static);
 }
 
-// int mystrchr(const char *src, int c)
-// {
-//     int i;
+int mystrchr(const char *src, int c)
+{
+    int i;
 
-//     i = 0;
-// 	while (src[i])
-// 	{
-// 		if (src[i] == c)
-// 			return(1);
-// 		i++;
-// 	}
-// 	return(0);
-// }
+    i = 0;
+	while (src[i])
+	{
+		if (src[i] == c)
+			return(1);
+		i++;
+	}
+	return(0);
+}
